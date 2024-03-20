89263844455a8c3e9a7802a40a1d15b63d249e78
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2024-03-20 19:43:56 -0300

    Added PVS comments
    I don't like them...

--- esclavo.c
+++ esclavo.c
@@ -1,22 +1,27 @@
+// This is a personal academic project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: https://pvs-studio.com
+
 #include <unistd.h>
 #include <stdio.h>
 #include <string.h>
 
-char * truncateString(char *s){
+char *truncateString(char *s)
+{
     int i = 0;
-    while(s[i] && s[i] != ' ' && s[i] != '\n')
+    while (s[i] && s[i] != ' ' && s[i] != '\n')
         i++;
-    s[i]=0;
+    s[i] = 0;
     return s;
 }
 
-int main(int argc, char ** argv){
-    if(argc < 2)
+int main(int argc, char **argv)
+{
+    if (argc < 2)
         return 1;
     char buffer[4096] = "md5sum ";
-    FILE * md5sum = popen(strcat(buffer,argv[1]),"r");
-    fgets(buffer,4096,md5sum);
-    write(1,buffer,strlen(buffer));
+    FILE *md5sum = popen(strcat(buffer, argv[1]), "r");
+    fgets(buffer, 4096, md5sum);
+    write(1, buffer, strlen(buffer));
     pclose(md5sum);
     return 0;
 }

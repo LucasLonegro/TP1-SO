b162b978bfe99f364630b02e517e7eaabe6e076b
LucasLonegro <llonegrogurfinkel@itba.edu.ar>
2024-03-20 19:34:15 -0300

    now removes filenames from md5 output

--- esclavo.c
+++ esclavo.c
@@ -2,12 +2,21 @@
 #include <stdio.h>
 #include <string.h>
 
+char * truncateString(char *s){
+    int i = 0;
+    while(s[i] && s[i] != ' ' && s[i] != '\n')
+        i++;
+    s[i]=0;
+    return s;
+}
+
 int main(int argc, char ** argv){
     if(argc < 2)
         return 1;
     char buffer[4096] = "md5sum ";
     FILE * md5sum = popen(strcat(buffer,argv[1]),"r");
     fgets(buffer,4096,md5sum);
+    truncateString(buffer);
     write(1,buffer,strlen(buffer));
     pclose(md5sum);
     return 0;

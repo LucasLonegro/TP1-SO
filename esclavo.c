c5c14c30885967de94349f3f91f65945f10eb1d2
LucasLonegro <llonegrogurfinkel@itba.edu.ar>
2024-03-20 21:22:54 -0300

    we're good now

--- esclavo.c
+++ esclavo.c
@@ -18,8 +18,8 @@ int main(int argc, char **argv)
 {
     if (argc < 2)
         return 1;
-    char buffer[4096] = "md5sum ";
-    snprintf(buffer + strlen(buffer), sizeof(buffer) - strlen(buffer), "%s", argv[1]);
+    char buffer[4096];
+    snprintf(buffer, sizeof(buffer), "md5sum %s", argv[1]);
     FILE *md5sum = popen(buffer, "r");
     fgets(buffer, 4096, md5sum);
     write(1, buffer, strlen(buffer));

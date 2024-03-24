be8aee610e351a3e467beb00275071751e5971b3
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2024-03-24 17:00:03 -0300

    QoL changes
    Renamed variables and added more documentation

--- esclavo.c
+++ esclavo.c
@@ -18,13 +18,23 @@ void removeNewLine(char *s);
 
 int main(int argc, char *argv[])
 {
-    char stdinBuffer[MAX_PATH] = {0};
-    char cmd[MAX_PATH + sizeof("md5sum ") - 1];
+    /**
+     * @brief The input string
+     */
+    char input[MAX_PATH] = {0};
+    /**
+     * @brief The command string to execute
+     */
+    char cmd[sizeof("md5sum ") - 1 /* String without \0 */ + MAX_PATH];
+    /**
+     * @brief The output string
+     */
+    char output[32 /* Hash */ + 2 /* Spaces */ + MAX_PATH + 1 /* \0 */];
 
     while (1)
     {
-        int n = read(STDIN_FILENO, stdinBuffer, sizeof(stdinBuffer));
-        if (n == sizeof(stdinBuffer) || n < 0)
+        int n = read(STDIN_FILENO, input, sizeof(input));
+        if (n == sizeof(input) || n < 0)
         {
             // Unexpected error
             write(STDOUT_FILENO, "", 1);
@@ -37,26 +47,26 @@ int main(int argc, char *argv[])
 
         // Check whether the given file can be read in order to calculate its md5 hash
         struct stat fileData;
-        if (stat(stdinBuffer, &fileData) || S_ISDIR(fileData.st_mode))
+        if (stat(input, &fileData) || S_ISDIR(fileData.st_mode))
         {
             // Inform that the path given did not refer to a file this process could read
-            int len = snprintf(cmd, sizeof(cmd), "Could not read: %s", stdinBuffer);
+            int len = snprintf(cmd, sizeof(cmd), "Could not read: %s", input);
             write(STDOUT_FILENO, cmd, len + 1);
 
             continue;
         }
 
         // Write the verified readable file into an md5sum command
-        snprintf(cmd, sizeof(cmd), "md5sum %s", stdinBuffer);
+        snprintf(cmd, sizeof(cmd), "md5sum %s", input);
 
         // Call md5sum
         FILE *md5sum = popen(cmd, "r");
 
         // Parse the md5sum output and write it into stdout
-        if (fgets(cmd, sizeof(cmd), md5sum))
+        if (fgets(output, sizeof(output), md5sum))
         {
-            removeNewLine(cmd);
-            write(STDOUT_FILENO, cmd, strlen(cmd) + 1);
+            removeNewLine(output);
+            write(STDOUT_FILENO, output, strlen(output) + 1);
         }
 
         pclose(md5sum);

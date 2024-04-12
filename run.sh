d66497836ac39103592c757a465fd3c08151861d
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2024-04-11 22:04:40 -0300

    Accidentaly commited to master

--- run.sh
+++
@@ -1,4 +0,0 @@
-# docker run --rm -v "${PWD}:/app" --privileged agodio/itba-so:2.0 /bin/make -C /app [command]
-docker run --rm -v "${PWD}:/app" --privileged agodio/itba-so:2.0 /bin/make -C /app all
-# docker run --rm -v "${PWD}:/app" --privileged agodio/itba-so:2.0 /bin/make -C /app valgrind
-# docker run --rm -v "${PWD}:/app" --privileged agodio/itba-so:2.0 /bin/make -C /app pvs
